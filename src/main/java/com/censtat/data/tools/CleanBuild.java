2d35303a438f8edf32ca9af5d04941771eb6ce93
Rafi Shaik <srafi7399@gmail.com>
2016-10-12 10:46:23 -0700

    Added script to clean the build

---
+++ src/main/java/com/censtat/data/tools/CleanBuild.java
@@ -0,0 +1,69 @@
+package com.censtat.data.tools;
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
+public class CleanBuild {
+	
+	
+	public static void cleanUpGeneratedFilesAndDirectories() {
+		
+		
+		String targetDir = System.getProperty("user.dir")
+								+File.separator+"src"
+								+File.separator+"main"
+								+File.separator+"webapp"
+								+File.separator+"WEB-INF"
+								+File.separator+"views"
+								+File.separator+"tiles"
+								+File.separator+"states";
+		
+		String tilesDir =  System.getProperty("user.dir")
+				+File.separator+"src"
+				+File.separator+"main"
+				+File.separator+"webapp"
+				+File.separator+"WEB-INF"
+				+File.separator+"views"
+				+File.separator+"tiles";
+		
+		String states = tilesDir +File.separator+"states.jsp";
+				
+		
+		File file = new File(targetDir);
+		if(!file.isDirectory()) {
+			System.out.println("The States Directory Does not Exist");
+			System.out.println("Creating.....");
+			try{
+				file.mkdir();				
+			}catch(Exception e) {
+				System.out.println(e);				
+			}
+		}
+		else {
+			try {
+				FileUtils.deleteDirectory(file);
+				System.out.println("Sucessfully Deleted");
+				
+			} catch (Exception e) {
+				System.out.println(e);
+
+			}			
+			
+		}
+		
+		//Delete the states.jsp file
+		File statesFile = new File(states);
+		if(statesFile.exists()){
+			if(statesFile.delete()) {
+				System.out.println("Sucessfully Deleted the States File ");
+			}
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {		
+		CleanBuild.cleanUpGeneratedFilesAndDirectories();
+	}
+
+}
